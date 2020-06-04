bf9a14f5d61c3d7784b128dd9c670446388ae59e
roycechua23 <royce236@gmail.com>
2020-06-05 00:24:47 +0800

    Add update functionality to react app

--- postit/src/components/post.js
+++ postit/src/components/post.js
@@ -7,12 +7,39 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { useForm } from "react-hook-form";
 
 export default function Post(props) {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const [title, setTitle] = React.useState(props.title)
+    const [message, setMessage] = React.useState(props.message)
+    const [open, setOpen] = React.useState(false);
+
+    // handle Modal opening and closing
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     const useStyles = makeStyles((theme) => ({
         root: {
-          
+            
         },
+        modal: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+
     }));
 
     const handleDelete = function () {
@@ -22,9 +49,24 @@ export default function Post(props) {
             }).then(function(response) {
                 return response.json();
             }).then(function(data) {
-                alert("Delete button pressed:" + props.id);
             });
             
+    }
+
+    const handleUpdate = function (data) {
+        fetch('http://127.0.0.1:5000/posts/' + props.id, {
+            mode: 'cors',
+            method: 'PUT',
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(data)
+        }).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+           
+        });
+        
     }
 
     const classes = useStyles();
@@ -40,12 +82,54 @@ export default function Post(props) {
                 </Typography>
                 </CardContent>
             <CardActions>
-                <Button size="small" style={{color:"green"}}>
+                <Button size="small" style={{color:"green"}} onClick={handleOpen}>
                 Update
                 </Button>
                 <Button size="small" color="secondary" onClick={handleDelete}>
                 Delete
                 </Button>
+
+                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Update Post</DialogTitle>
+                    <DialogContent>
+                    <form onSubmit={handleSubmit(handleUpdate)} className={classes.root} noValidate autoComplete="off">
+                            <p>
+                                <TextField
+                                    name="title"
+                                    inputRef={register({required: true})}
+                                    id="filled-textarea"
+                                    label="Title"
+                                    placeholder="Put your title here"
+                                    defaultValue={title}
+                                    onChange={(new_val) => setTitle(new_val)}
+                                />
+                            </p>
+                            <p>
+                                <TextField
+                                    name="message"
+                                    inputRef={register({required: true})}
+                                    id="filled-textarea"
+                                    label="Message"
+                                    placeholder="Put your message here"
+                                    rows={4}
+                                    multiline
+                                    defaultValue={message}
+                                    onChange={(new_val) => setTitle(new_val)}
+                                />
+                            </p>
+                            <p>
+                                <Button type="submit" fullWidth={true} variant="outlined" color="primary" onClick={()=>handleClose()}>
+                                    Update Message
+                                </Button>
+                            </p>
+                    </form>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                    </DialogActions>
+                </Dialog>
             </CardActions>
         </Card>
     );
